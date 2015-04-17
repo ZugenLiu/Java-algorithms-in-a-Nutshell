4f1fe5c2794636afde7744d070aeca5cc2ef4225
zaleslaw <zaleslaw.sin@gmail.com>
2015-04-17 12:27:00 +0300

    Implemented CountingSort. Closed #5

---
+++ src/Sorting/CountingSort.java
@@ -0,0 +1,41 @@
+package Sorting;
+
+
+public class CountingSort {
+
+    /*Sort the n elements in ar, drawn from the values [0,k). */
+    public static int[] sort(int[] a, int k) {
+
+        int[] B = new int[k];
+
+        for (int number : a) {
+            B[number]++;
+        }
+
+        int idx = 0;
+
+        for (int i = 0; i < k; i++) {
+            while (B[i]-- > 0) {
+                a[idx++] = i;
+            }
+        }
+        return a;
+    }
+
+    /* Sort the n elements in ar, drawn from the values [min, max). */
+    public static int[] sortIntegersFromRange(int[] a, int min, int max) {
+        int[] B = new int[max - min + 1];
+
+        for (int number : a) {
+            B[number - min]++;
+        }
+
+        int idx = 0;
+        for (int i = min; i <= max; i++) {
+            while (B[i - min]-- > 0) {
+                a[idx++] = i;
+            }
+        }
+        return a;
+    }
+}
---
+++ src/Sorting/CountingSortTest.java
@@ -0,0 +1,29 @@
+package Sorting;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class CountingSortTest {
+
+    @Test
+    public void testSortMethod() {
+
+        int[] a = {3, 0, 3, 0, 0, 2, 2};
+        a = CountingSort.sort(a, 4);
+        final int[] waitedResult = {0, 0, 0, 2, 2, 3, 3};
+        assertArrayEquals(waitedResult, a);
+
+    }
+
+    @Test
+    public void testSecondSortMethod() {
+
+        int[] a = {6, 5, 4, 7, 4, 5, 10};
+        a = CountingSort.sortIntegersFromRange(a, 4, 10);
+        final int[] waitedResult = {4, 4, 5, 5, 6, 7, 10};
+        assertArrayEquals(waitedResult, a);
+
+    }
+}
