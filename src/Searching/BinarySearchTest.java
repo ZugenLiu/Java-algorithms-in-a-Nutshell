bac92ee73bc04d85682a21d1cbe3d2d4654d2d3f
zaleslaw <zaleslaw.sin@gmail.com>
2015-04-18 14:23:39 +0300

    Implemented BinarySearch. Closed #9

---
+++ src/Searching/BinarySearchTest.java
@@ -0,0 +1,20 @@
+package Searching;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class BinarySearchTest {
+
+    @Test
+    public void testSearchMethod() {
+
+        Integer[] a = {1, 4, 8, 9, 11, 15, 17};
+        assertTrue(new BinarySearch<Integer>().search(a, 11));
+        assertFalse(new BinarySearch<Integer>().search(a, 100));
+
+    }
+
+}
