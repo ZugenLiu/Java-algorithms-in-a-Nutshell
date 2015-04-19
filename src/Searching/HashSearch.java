4275a073e315b337432ef7e2c7296bc0c01422d5
zaleslaw <zaleslaw.sin@gmail.com>
2015-04-19 11:19:40 +0300

    Implemented HashSearch. Closed #10

---
+++ src/Searching/HashSearch.java
@@ -0,0 +1,46 @@
+package Searching;
+
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class HashSearch<V> {
+    private LinkedList<V>[] listTable;
+    private int tableSize;
+
+
+    public void load(Iterator<V> it, int size) {
+        tableSize = size;
+        listTable = (LinkedList<V>[]) new LinkedList[tableSize];
+        // Pull each value from the iterator and find desired bin h.
+        // Add to existing list or create new one into which value is added.
+        while (it.hasNext()) {
+            V v = it.next();
+            int h = this.hash(v);
+            if (listTable[h] == null) {
+                listTable[h] = new LinkedList<V>();
+            }
+            // Add element into linked list for bin 'h'
+            LinkedList<V> list = (LinkedList<V>) listTable[h];
+            list.add(v);
+        }
+    }
+
+    public boolean search(V v) {
+        int h = this.hash(v);
+        LinkedList<V> list = (LinkedList<V>) listTable[h];
+        if (list == null) {
+            return false;
+        }
+        return list.contains(v);
+    }
+
+    // The following is the implementation of the hash method above.
+    public int hash(V v) {
+        int h = v.hashCode();
+        if (h < 0) {
+            h = 0 - h;
+        }
+        return h % tableSize;
+    }
+}
---
+++ src/Searching/HashSearchTest.java
@@ -0,0 +1,27 @@
+package Searching;
+
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class HashSearchTest {
+
+    @Test
+    public void testSearchMethod() {
+
+
+        HashSearch<Integer> hashSearcher = new HashSearch<Integer>();
+        Iterator<Integer> iterator = new ArrayList<Integer>(Arrays.asList(1, 4, 8, 9, 11, 15, 17)).iterator();
+        hashSearcher.load(iterator, 3);
+        assertTrue(hashSearcher.search(11));
+        assertFalse(hashSearcher.search(100));
+
+    }
+
+}
